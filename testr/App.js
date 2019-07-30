0ca7bfc8be67eac3516c286e3b231fa453a46fca
Nick-2013 <nick-2013@live.com>
2019-07-30 15:35:55 -0500

    it works 2

--- testr/App.js
+++ testr/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePage from './screens/HomePage'
 
 export default function App() {
   return (
     <View style={styles.container} >
       <Text style={{color:"red", fontWeight: 'bold'}} >The quick brown fox jumps over the lazy dog</Text>
+      <HomePage />
     </View>
+
   );
 }
 
---
+++ testr/screens/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function HomePage() {
+    return (
+      <View>
+        <Text style={{color:"cyan"}} >AAAAAAAAAAAAAAAAAAAAAAAAAAA</Text>
+      </View>
+    );
+  }
+  
